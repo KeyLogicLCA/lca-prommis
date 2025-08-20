fb507a6ef0d34f226180c1468a7526156c4cdc3a
Francis Hanna <hannafra@msu.edu>
2025-08-20 12:34:13 -0400

    Update 08/20/2025: create_new_process.py and find_processes_by_flow.py

--- src/create_olca_process/find_processes_by_flow.py
+++ src/create_olca_process/find_processes_by_flow.py
@@ -33,7 +33,7 @@ import pandas as pd
 def find_processes_by_flow(exchanges_df, flow_uuid: str):
 
     # dataframe containing exchanges
-    df = exchanges_df.copy()
+    df = exchanges_df
 
     # filter rows from the database that have a flow uuid that matches the flow_uuid
     df.drop(df[df['exchange_uuid'] != flow_uuid].index, inplace=True)
