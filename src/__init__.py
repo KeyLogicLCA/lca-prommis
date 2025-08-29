ae52ae15364719c591d6763bd72d7b6c79ded7e9
Francis Hanna <hannafra@msu.edu>
2025-08-29 12:35:51 -0400

    updated init file

--- src/__init__.py
+++ src/__init__.py
@@ -3,4 +3,5 @@ import src.create_olca_process as create_lca
 import src.prommis_LCA_conversions as convert_lca
 import src.prommis_LCA_data as data_lca
 import src.create_ps as create_ps
- 
+import src.run_analysis as run_analysis
+import src.generate_total_results as generate_total_results
